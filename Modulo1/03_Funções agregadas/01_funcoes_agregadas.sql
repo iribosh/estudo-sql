19bf777278a6992c040428b03815090883692e0c
Marina <marina.iriboshi@outlook.com>
2023-05-18 11:33:11 -0300

    primeiros modulos

---
+++ Modulo1/03_Funções agregadas/01_funcoes_agregadas.sql
@@ -0,0 +1,58 @@
+-- PARA QUE SERVE ##################################################################
+-- Servem para executar operações aritmética nos registros de uma coluna 
+
+
+-- TIPOS DE FUNÇÕES AGREGADAS ######################################################
+-- COUNT()
+-- SUM()
+-- MIN()
+-- MAX()
+-- AVG()
+
+
+-- EXEMPLOS ########################################################################
+
+-- COUNT() -------------------------------------------------------------------------
+
+-- (Exemplo 1) Contagem de todas as linhas de uma tabela
+-- Conte todas as visitas realizadas ao site da empresa fictícia
+select 
+COUNT(*) 
+from sales.funnel;
+
+-- (Exemplo 2) Contagem das linhas de uma coluna
+-- Conte todos os pagamentos registrados na tabela sales.funnel 
+select 
+COUNT(paid_date) 
+from sales.funnel;
+
+-- (Exemplo 3) Contagem distinta de uma coluna
+-- Conte todos os produtos distintos visitados em jan/21
+select 
+COUNT (distinct product_id) 
+from sales.funnel
+where visit_page_date between '2021-01-01' and '2021-02-01';
+
+-- OUTRAS FUNÇÕES ------------------------------------------------------------------
+
+-- (Exemplo 4) Calcule o preço mínimo, máximo e médio dos productos da tabela products
+select 
+COUNT (distinct product_id) 
+from sales.funnel
+where visit_page_date between '2021-01-01' and '2021-02-01';
+
+-- (Exemplo 5) Informe qual é o veículo mais caro da tabela products
+
+
+-- RESUMO ##########################################################################
+-- (1) Servem para executar operações aritmética nos registros de uma coluna 
+-- (2) Funções agregadas não computam células vazias (NULL) como zero
+-- (3) Na função COUNT() pode-se utilizar o asterisco (*) para contar os registros
+-- (4) COUNT(DISTINCT ) irá contar apenas os valores exclusivos
+
+
+
+
+
+
+
